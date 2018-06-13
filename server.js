ccefac4953f1d21c7708a7e011efaf7f69592970
ygordantas <ygord.furtado@gmail.com>
2018-06-13 11:18:26 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+let app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("unable to append to server.log");
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    bodyText: "Welcome to the website"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    bodyText: "about page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Sorry we were unable to complete your request "
+  });
+});
+app.listen(3000);
